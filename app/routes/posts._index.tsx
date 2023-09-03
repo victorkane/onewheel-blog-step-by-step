2141865002663a998c3d7889833af1401951e63a
victorkane <victorkane@gmail.com>
2023-09-03 18:05:56 -0300

    lesson 04 scaffold posts page to list posts

--- app/routes/posts._index.tsx
+++ app/routes/posts._index.tsx
@@ -1,7 +1,28 @@
+import { Link } from "@remix-run/react";
 export default function PostsRoute() {
+  const posts = [
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "trail-riding-with-onewheel",
+      title: "Trail Riding with Onewheel",
+    },
+  ];
+
   return (
     <main>
-      <h1>Posts</h1>
+      <h1>The Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
