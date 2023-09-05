2bec374cdb8b53d1d013e597fb0e9a9ad3cc714b
victorkane <victorkane@gmail.com>
2023-09-05 12:23:52 -0300

    lesson 07 model app data in prisma schema, seed db and fetch data from db

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,18 +1,5 @@
+import { prisma } from "~/db.server";
 import { json } from "@remix-run/node";
 export async function getPosts() {
-  const posts = [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "trail-riding-with-onewheel",
-      title: "Trail Riding with Onewheel",
-    },
-    {
-      slug: "obelisk-in-buenos-aires",
-      title: "Obelisk in Buenos Aires",
-    },
-  ];
-  return posts;
+  return prisma.post.findMany();
 }
