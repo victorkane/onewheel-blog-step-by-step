ebec77f59f4899167272d83fd3a83752e72d65f8
victorkane <victorkane@gmail.com>
2023-09-05 10:29:37 -0300

    build: lesson 06 Integrate Typescript into a Remix Loader Function

--- app/routes/posts._index.tsx
+++ app/routes/posts._index.tsx
@@ -1,14 +1,19 @@
 import { Link, useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { getPosts } from "~/models/post.server";
 
-export const loader = async () => {
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>;
+};
+
+export const loader: LoaderFunction = async () => {
   const posts = await getPosts();
-  return json({ posts });
+  return json<LoaderData>({ posts });
 };
 
 export default function PostsRoute() {
-  const { posts } = useLoaderData();
+  const { posts } = useLoaderData() as LoaderData;
 
   return (
     <main>
