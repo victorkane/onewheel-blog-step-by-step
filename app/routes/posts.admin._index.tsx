1c4d9b5aabe536011ea28cd7de8d238c50ac367d
victorkane <victorkane@gmail.com>
2023-09-06 05:47:10 -0300

    lesson 12: Scaffold nested routes rendering in Remix

---
+++ app/routes/posts.admin._index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "@remix-run/react";
+export default function AdminIndexRoute() {
+  return (
+    <p>
+      <Link to="new" className="text-blue-600 underline">
+        Create a New Post
+      </Link>
+    </p>
+  );
+}
